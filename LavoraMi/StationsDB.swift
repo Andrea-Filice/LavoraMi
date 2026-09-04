85ddc3d589bd9430e9ee920aaa8ba84669ab9a21
Andrea Filice <filiceandrea8@gmail.com>
2026-09-04 10:27:39 +0200

    fix: fixing names
    * fixed names for "Cinisello Monza" new station

--- LavoraMi/StationsDB.swift
+++ LavoraMi/StationsDB.swift
@@ -11,7 +11,7 @@ import MapKit
 struct StationsDB {
     //MARK: METRO STATIONS:
     static let stationsM1: [MetroStation] = [
-        .init(name: "Monza Bettola", coordinate: .init(latitude: 45.55612, longitude: 9.24795), branch: "Nord - New"),
+        .init(name: "Cinisello Monza", coordinate: .init(latitude: 45.55612, longitude: 9.24795), branch: "Nord - New"),
         .init(name: "Sesto Restellone", coordinate: .init(latitude: 45.54841, longitude: 9.24268), branch: "Nord - New"),
         .init(name: "Sesto 1° Maggio FS", coordinate: .init(latitude: 45.54156, longitude: 9.23835), branch: "Main"),
         .init(name: "Sesto Rondò", coordinate: .init(latitude: 45.5342, longitude: 9.23128), branch: "Main"),
