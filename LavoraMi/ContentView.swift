cc3eb70af4b794cdb4e9631bce7fa3a967705f5e
Andrea Filice <filiceandrea8@gmail.com>
2026-09-04 11:40:27 +0200

    fix: fixing colors
    * fixed colors for "REG" lines

--- LavoraMi/ContentView.swift
+++ LavoraMi/ContentView.swift
@@ -8600,11 +8600,12 @@ func getColor(for line: String) -> Color {
         case "MXP1": return Color(red: 140/255, green: 0, blue: 118/255)
         case "MXP2": return Color(red: 140/255, green: 0, blue: 118/255)
         case "SFM 6": return Color(red: 139/255, green: 35/255, blue: 29/255)
+        case "REG": return Color(red: 226/255, green: 62/255, blue: 62/255)
         case "AV": return .red
         case "RV": return .red
         case String(localized: .aereoporto): return .cyan
         case _ where line.contains("R") && !line.contains("RE"): return Color.blue
-        case _ where line.contains("RE"): return Color.red
+        case _ where line.contains("RE") && !line.contains("REG"): return Color.red
         case let s where (1...33).contains(Int(s) ?? 0): return .orange
         case _ where line.starts(with: "N"): return Color(red: 2/255, green: 27/255, blue: 129/255)
         
